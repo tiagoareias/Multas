7216f73d30bc975980303c2e015f3bb85c72052f
André Carvalho <afecarvalho@gmail.com>
2018-04-25 11:15:12 +0100

    Adicionar ficheiros Web API

---
+++ Multas-tA/Multas-tA/Api/AgentesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Multas_tA.Models;
+
+namespace Multas_tA.Api
+{
+    public class AgentesController : ApiController
+    {
+        private MultasDb db = new MultasDb();
+
+        // GET: api/Agentes
+        public IQueryable<Agentes> GetAgentes()
+        {
+            return db.Agentes;
+        }
+
+        // GET: api/Agentes/5
+        [ResponseType(typeof(Agentes))]
+        public IHttpActionResult GetAgentes(int id)
+        {
+            Agentes agentes = db.Agentes.Find(id);
+            if (agentes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(agentes);
+        }
+
+        // PUT: api/Agentes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAgentes(int id, Agentes agentes)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != agentes.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(agentes).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgentesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Agentes
+        [ResponseType(typeof(Agentes))]
+        public IHttpActionResult PostAgentes(Agentes agentes)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Agentes.Add(agentes);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (AgentesExists(agentes.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = agentes.ID }, agentes);
+        }
+
+        // DELETE: api/Agentes/5
+        [ResponseType(typeof(Agentes))]
+        public IHttpActionResult DeleteAgentes(int id)
+        {
+            Agentes agentes = db.Agentes.Find(id);
+            if (agentes == null)
+            {
+                return NotFound();
+            }
+
+            db.Agentes.Remove(agentes);
+            db.SaveChanges();
+
+            return Ok(agentes);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AgentesExists(int id)
+        {
+            return db.Agentes.Count(e => e.ID == id) > 0;
+        }
+    }
+}
