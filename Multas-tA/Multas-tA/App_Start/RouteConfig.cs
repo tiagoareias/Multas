e76e26e10ba19889597278936388af5a6eea395e
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2018-05-04 15:00:15 +0100

    adição das classes de Autenticação

--- Multas-tA/Multas-tA/App_Start/RouteConfig.cs
+++ Multas-tA/Multas-tA/App_Start/RouteConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Multas_tA
+namespace IdentitySample
 {
     public class RouteConfig
     {
@@ -13,11 +9,10 @@ namespace Multas_tA
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // definir os valores a serem utilizados na Rota, por defeito
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Agentes", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
